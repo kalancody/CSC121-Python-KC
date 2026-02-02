3e636f2a102764a5806da59269d56c09f0e1207d
kalancody <kalangcody@students.abtech.edu>
2026-02-02 10:53:20 -0500

    Add files via upload

---
+++ 3.3 My Own List.py
@@ -0,0 +1,5 @@
+vehicles = ['Honda Civic', 'Rivian R1T', 'Bmw m3', 'Cessna 172']
+print(f"I currently drive a {vehicles[0]}.")
+print(f"I would like to own a {vehicles[1]} at some point in my life.")
+print(f"My dream car is a {vehicles[2].upper()}.")
+print(f"I would also like to own a {vehicles[3]} after I get my pilot's license.")
