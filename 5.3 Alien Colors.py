40caba0ad4d7bc80d3b8188fd4ccb79b543e7f14
kalancody <kalangcody@students.abtech.edu>
2026-02-09 14:30:21 -0500

    Add files via upload

---
+++ 5.3 Alien Colors.py
@@ -0,0 +1,26 @@
+# Inital prompt.
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+elif alien_color == 'yellow':
+    print("You just earned 10 points!")
+elif alien_color == 'red':
+    print("You just earned 15 points!")
+
+# Final version with revision.
+alien_color = 'blue'
+
+if alien_color == 'green':
+    score = 5
+elif alien_color == 'yellow':
+    score = 10
+elif alien_color == 'red':
+    score = 15
+else:
+    score = 0
+
+if score >= 5:
+    print(f"You just earned {score} points!")
+else:
+    print("Error, you earned zero points!")
