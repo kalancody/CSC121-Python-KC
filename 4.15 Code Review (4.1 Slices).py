8d37d605928392eed9487e0111d934039f1ac3c5
kalancody <kalangcody@students.abtech.edu>
2026-02-16 14:45:18 -0500

    Add files via upload

---
+++ 4.15 Code Review (4.1 Slices).py
@@ -0,0 +1,27 @@
+# Initial guest list.
+guests = ['steve jobs', 'abraham lincoln', 'franklin roosevelt']
+print(f"{guests[0].title()}, you are invited to my party!")
+print(f"{guests[1].title()}, you are invited to my party!")
+print(f"{guests[2].title()}, you are invited to my party!")
+
+# Revisions to guest list.
+print(
+    f"{guests[0].title()}, {guests[1].title()}, and {guests[2].title()}, "
+      "I have found a bigger dinner table, so I will invite more guests.")
+
+guests.insert(0, "Lionel Messi")
+guests.insert(2, "Winston Churchill")
+guests.append("Drake")
+guests.sort(key=str.lower)
+
+# New invitations are sent out.
+for guest in guests:
+    print(f"{guest.title()}, you are invited to my larger party!")
+
+# Additional code for slices assignment.
+print("The first three items in the list are:")
+print(guests[:3])
+print("Three items from the middle of the list are:")
+print(guests[1:4])
+print("The last three items in the list are:")
+print(guests[3:])
---
+++ 4.15 Code Review (4.2 Animals).py
@@ -0,0 +1,9 @@
+
+animals = ["cat", "dog", "rabbit", "hamster", "horse"]
+
+for animal in animals:
+    print(f"A {animal} would make a great pet.")
+
+print(
+    f"All of these animals are mammals. The {animals[3]} is the smallest, "
+    f"and the {animals[4]} is the largest.")
