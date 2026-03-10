4a3a1df9610c4916a0ccc0a9cc595b9a83013b32
kalancody <kalangcody@students.abtech.edu>
2026-03-10 11:50:35 -0400

    Add files via upload

---
+++ 8.5 Cities.py
@@ -0,0 +1,6 @@
+def describe_city(city, country='a country somewhere on earth'):
+    print(f"{city.title()} is in {country}.")
+
+describe_city('Brisbane', 'Australia')
+describe_city('Berlin', 'Germany')
+describe_city('London')
